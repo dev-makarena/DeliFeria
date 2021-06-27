9563831cb7309b71dc275b3909a10a02d38cb82b
francoBg1 <franco9bernal@gmail.com>
2021-06-27 17:47:54 -0400

    para pruebas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>Deliferia</title>
+    <link rel="shortcut icon" href="{{ url('image/frutas.png') }}" type="image/x-icon">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
